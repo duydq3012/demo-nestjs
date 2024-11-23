6ffc1ba753c07f86c3fce6cc48eafe7dbd9dc24a
DuyDQ <duydq3012@gmail.com>
2024-11-23 23:07:51 +0700

    set sleep

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello (): Promise<string> {
+    await sleep(1000)
     return 'Hello World!';
   }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
